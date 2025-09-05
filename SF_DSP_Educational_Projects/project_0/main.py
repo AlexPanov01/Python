72ccdeb5148d8670e13ef79e9d66afd0cdbd12a1
AlexPanov01 <alex.panov0289@gmail.com>
2025-09-05 20:47:44 +0500

    new file:   SF_DSP_Educational_Projects/project_0/game_v1.py 	new file:   SF_DSP_Educational_Projects/project_0/game_v2.py 	modified:   SF_DSP_Educational_Projects/project_0/main.py 	new file:   SF_DSP_Educational_Projects/project_0/readme.md 	new file:   SF_DSP_Educational_Projects/project_0/requirments.txt

--- SF_DSP_Educational_Projects/project_0/main.py
+++ SF_DSP_Educational_Projects/project_0/main.py
@@ -1,18 +1,4 @@
-# Guess the number
-import numpy as np
+from game_v2 import score_game
+from game_v2 import random_predict
 
-number = np.random.randint(1, 101) # загадываем число
-count = 0
-
-while True:
-    count += 1
-    predict_number = int(input("Угадай число от 1 до 100: "))
-
-    if predict_number > number:
-        print("Число должно быть меньше!")
-
-    elif predict_number < number:
-        print("Число должно быть больше!")
-    else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break # end game, out of cycle
+score_game(random_predict)
